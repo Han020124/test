1186fa619b72022f1a9f5136663c865f73392c28
Han020124 <han3003ty@gmail.com>
2025-06-04 16:52:40 +0900

    Update routes.index.js

--- routes.index.js
+++ routes.index.js
@@ -4,13 +4,12 @@ const router = express.Router();
 const { getTableList } = require('../models/index');    
 
 router.get('/', async (req, res) => {
-    try {
-      
+    try { 
         const tableList = await getTableList();
-        
-        
         res.render('index', { tableList });
-    } catch (error) { 
+    } 
+    
+    catch (error) { 
         console.error('테이블 리스트 조회 중 오류가 발생하였습니다:', error);
         res.status(500).send('서버 오류가 발생했습니다.');
     }
